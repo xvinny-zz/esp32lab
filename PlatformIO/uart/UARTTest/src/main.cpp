f931d4fd144e1eae7fbba9adb26d7ce30caef99d
Vinicius de Sa <vinicius.sa@live.com>
2020-03-23 15:30:08 -0300

    Alterações na UART

--- PlatformIO/uart/UARTTest/src/main.cpp
+++ PlatformIO/uart/UARTTest/src/main.cpp
@@ -1,33 +1,45 @@
 #include <Arduino.h>
 
+#define SerialAT Serial1
+
+const gpio_num_t GSM_TX = GPIO_NUM_13;
+const gpio_num_t GSM_RX = GPIO_NUM_14;
+
 String inputBuffer;
+String outputBuffer;
+
 
 void setup() 
 {
     Serial.begin(115200);
-    Serial2.begin(115200);
+    SerialAT.begin(115200, SERIAL_8N1, GSM_TX, GSM_RX, false);
+    delay(500);
 
     inputBuffer = "";
+    outputBuffer = "";
     ESP_LOGI("MAIN", "Sistema iniciado. Aguardando comandos...");
   // put your setup code here, to run once:
 }
 
 void loop() 
 {
-    if (Serial2.available())
-    {
-        inputBuffer = Serial2.readStringUntil('\n');
+    while(Serial.available())
+        outputBuffer.concat((char)Serial.read());
 
-        ESP_LOGI("MAIN", "Recebido da Serial2: [%s]", inputBuffer.c_str());
+    if (outputBuffer.length() > 0)
+    {
+        SerialAT.print(outputBuffer);
+        outputBuffer = "";
     }
 
-    if (Serial.available())
-    {
-        inputBuffer = Serial.readStringUntil('\n');
+    while (SerialAT.available())
+        inputBuffer.concat((char)SerialAT.read());
 
-        Serial2.printf("Recebido da Serial: [%s]", inputBuffer.c_str());
+    if (inputBuffer.length() > 0)
+    {
+        Serial.print(inputBuffer);
+        inputBuffer = "";
     }
 
     vTaskDelay(10 / portTICK_PERIOD_MS);
-  // put your main code here, to run repeatedly:
 }
