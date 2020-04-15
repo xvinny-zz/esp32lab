6dac38593e3142506ce3a0aa1dc11d1f29803390
Vinicius de Sa <vinicius.sa@live.com>
2020-04-15 09:35:52 -0300

    Teste da UART aprimorado

--- PlatformIO/uart/UARTTest/src/main.cpp
+++ PlatformIO/uart/UARTTest/src/main.cpp
@@ -1,22 +1,27 @@
 #include <Arduino.h>
 
-#define SerialAT Serial1
+
+//#define SerialAux Serial1
+#define SerialAux Serial2
 
 const gpio_num_t GSM_TX = GPIO_NUM_13;
 const gpio_num_t GSM_RX = GPIO_NUM_14;
+// const gpio_num_t GPS_TX = GPIO_NUM_16;
+// const gpio_num_t GPS_RX = GPIO_NUM_17;
 
-String inputBuffer;
-String outputBuffer;
+String bufferSerialAux;
+String bufferSerial;
 
 
 void setup() 
 {
     Serial.begin(115200);
-    SerialAT.begin(115200, SERIAL_8N1, GSM_TX, GSM_RX, false);
+    //SerialAux.begin(115200, SERIAL_8N1, GSM_TX, GSM_RX, false);
+    SerialAux.begin(115200, SERIAL_8N1, GSM_TX, GSM_RX, false);
     delay(500);
 
-    inputBuffer = "";
-    outputBuffer = "";
+    bufferSerialAux = "";
+    bufferSerial = "";
     ESP_LOGI("MAIN", "Sistema iniciado. Aguardando comandos...");
   // put your setup code here, to run once:
 }
@@ -24,21 +29,23 @@ void setup()
 void loop() 
 {
     while(Serial.available())
-        outputBuffer.concat((char)Serial.read());
+        bufferSerial.concat((char)Serial.read());
 
-    if (outputBuffer.length() > 0)
+    if (bufferSerial.length() > 0)
     {
-        SerialAT.print(outputBuffer);
-        outputBuffer = "";
+        SerialAux.print(bufferSerial.c_str());
+        //Serial.printf("\nEnviado: %s", bufferSerial.c_str());
+        bufferSerial = "";
     }
 
-    while (SerialAT.available())
-        inputBuffer.concat((char)SerialAT.read());
+    while (SerialAux.available())
+        bufferSerialAux.concat((char)SerialAux.read());
 
-    if (inputBuffer.length() > 0)
+    if (bufferSerialAux.length() > 0)
     {
-        Serial.print(inputBuffer);
-        inputBuffer = "";
+        Serial.print(bufferSerialAux.c_str());
+        //SerialAux.printf("\nEnviado: %s", bufferSerialAux.c_str());
+        bufferSerialAux = "";
     }
 
     vTaskDelay(10 / portTICK_PERIOD_MS);
